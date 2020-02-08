09d7275352eca6f901fcbbd7cdc4a9b92ccd809c
Emanuel Sundin <emanuel.sundin@hotmail.se>
2020-02-08 19:12:55 +0100

    included funny map

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -16,8 +16,8 @@ const vm = new Vue({
       markDone: function() {
         let pName = document.getElementById("name").value;
         let pMail = document.getElementById("E-mail").value;
-        let pStreetName = document.getElementById("streetName").value;
-        let pStreetNumber = document.getElementById("number").value;
+        //let pStreetName = document.getElementById("streetName").value;
+        //let pStreetNumber = document.getElementById("number").value;
         let pPayment = document.getElementById("payment").value;
         
         
@@ -39,7 +39,7 @@ const vm = new Vue({
           }
         }
 
-        let dataArray = [pName, pMail, pStreetName, pStreetNumber, pPayment, burgerIndex, pGender];
+        let dataArray = [pName, pMail, pPayment, burgerIndex, pGender];
         console.log("Klick!" + pGender);
 
         let order = document.getElementById("recOrder");
@@ -47,36 +47,59 @@ const vm = new Vue({
       }
   }
 });
+'use strict';
+const socket = io();
 
-
-
-
-
-/*
-function burgerMaker(name, kCal){
-  this.name = name;
-  this.kCal = kCal;
-}
-
-let a = new burgerMaker("burgare 1", 300);
-let b = new burgerMaker("burgare 2", 210);
-
-let ol = document.getElementById("list");
-let li = document.createElement("li");
-let liText = document.createTextNode((a.name));
-li.appendChild(liText);
-ol.appendChild(li);
-
-new Vue({
-  el: '#app',
+/* eslint-disable-next-line no-unused-vars */
+const vm = new Vue({
+  el: '#dots',
   data: {
-    burgers: [a, b]
+    orders: {},
+  },
+  created: function() {
+    /* When the page is loaded, get the current orders stored on the server.
+     * (the server's code is in app.js) */
+    socket.on('initialize', function(data) {
+      this.orders = data.orders;
+    }.bind(this));
+
+    /* Whenever an addOrder is emitted by a client (every open map.html is
+     * a client), the server responds with a currentQueue message (this is
+     * defined in app.js). The message's data payload is the entire updated
+     * order object. Here we define what the client should do with it.
+     * Spoiler: We replace the current local order object with the new one. */
+    socket.on('currentQueue', function(data) {
+      this.orders = data.orders;
+    }.bind(this));
   },
   methods: {
-    name: function() {
-      return this.burgers;
-    }
-  }
-})
-
-*/
+    getNext: function() {
+      /* This function returns the next available key (order number) in
+       * the orders object, it works under the assumptions that all keys
+       * are integers. */
+      let lastOrder = Object.keys(this.orders).reduce(function(last, next) {
+        return Math.max(last, next);
+      }, 0);
+      return lastOrder + 1;
+    },
+    addOrder: function(event) {
+      /* When you click in the map, a click event object is sent as parameter
+       * to the function designated in v-on:click (i.e. this one).
+       * The click event object contains among other things different
+       * coordinates that we need when calculating where in the map the click
+       * actually happened. */
+      let offset = {
+        x: event.currentTarget.getBoundingClientRect().left,
+        y: event.currentTarget.getBoundingClientRect().top,
+      };
+      socket.emit('addOrder', {
+        orderId: this.getNext(),
+        details: {
+          x: event.clientX - 10 - offset.x,
+          y: event.clientY - 10 - offset.y,
+        },
+        orderItems: ['Beans', 'Curry'],
+      });
+    },
+  },
+});
